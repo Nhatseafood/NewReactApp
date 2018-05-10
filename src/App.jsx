bb537ecfb883cf648324f815aeabb4f9f63d782a
Nhat Nguyen <nguyenhnhat@gmail.com>
2018-05-10 13:34:23 -0500

    clearing app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+       <h1>REACT APP</h1>
+       </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
